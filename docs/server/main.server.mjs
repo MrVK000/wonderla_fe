79d310d96690e65b1a81860a540a83e224ec7484
VK <vaikundamuthu1430@gmail.com>
2024-08-20 17:05:19 +0530

    Removed docs folder

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-NTSL5X6K.mjs";import"./chunk-PBBLLUPS.mjs";import"./chunk-KQQL5FXL.mjs";import"./chunk-7V4JJFTF.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
