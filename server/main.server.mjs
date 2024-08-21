ddc92a9173685645b614ccf5dda2299b7cdedc2c
VK <vaikundamuthu1430@gmail.com>
2024-08-21 12:25:42 +0530

    removed unwanted files

--- server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-ER5WWV62.mjs";import"./chunk-HPWWRZ6K.mjs";import"./chunk-KSWG5CEK.mjs";import"./chunk-R7PR3OSB.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
